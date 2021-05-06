c8c05f1afa619f0ab693942cd1fc936b4dc0cc77
vahid-bagh3ri <70237190+vahid-bagh3ri@users.noreply.github.com>
2021-05-06 21:17:05 +0430

    starting-project

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
 function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
+    const enteredValue = inputEl.value
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEl.appendChild(listItemEl)
+    inputEl.value = ''
 }
 
 buttonEl.addEventListener('click', addGoal);
-
-
-
-
