a4803535182e319ad42c0eed6cefd69522495fb4
vahid-bagh3ri <70237190+vahid-bagh3ri@users.noreply.github.com>
2021-05-08 09:11:04 +0430

    basics-03-events-starting-code

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: "finish the course and learn vue!",
-      courseGoalB: "master vue and builing amazing apps!",
-      vueLink: "https://vuejs.org/",
+      counter: 0,
     };
-  },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
-    },
   },
 });
 
-app.mount("#user-goal");
+app.mount('#events');
