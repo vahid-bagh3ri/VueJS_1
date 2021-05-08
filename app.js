fcd28292e2c85e59b512f14e3be4dc9489fc414f
vahid-bagh3ri <70237190+vahid-bagh3ri@users.noreply.github.com>
2021-05-08 10:39:57 +0430

    14. Exploring Event Modifiers

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
+      confirmedName: "",
     };
   },
-  methods:{
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      alert("Submitted!");
+    },
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value + " " + lastName;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +23,8 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--
-    }
-  }
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
