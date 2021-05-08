43cf92fce7db5240e583dde68d62018f270c5c71
vahid-bagh3ri <70237190+vahid-bagh3ri@users.noreply.github.com>
2021-05-08 11:45:36 +0430

    17. Data Binding + Event Binding = Two-Way Binding

--- app.js
+++ app.js
@@ -7,15 +7,8 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    confirmInput() {
-      this.confirmedName = this.name;
-    },
-    submitForm(event) {
-      // event.preventDefault();
-      alert("Submitted!");
-    },
     setName(event, lastName) {
-      this.name = event.target.value + " " + lastName;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -23,6 +16,9 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--
+    },
+    resetInput() {
+      this.name = '';
     },
   },
 });
