f36b517f6bb481df230301644317efa07d64de0a
vahid-bagh3ri <70237190+vahid-bagh3ri@users.noreply.github.com>
2021-05-10 20:39:58 +0430

    3. Updating the Health Bars

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const app = Vue.createApp({
       monsterHealth: 100,
     };
   },
-
+  computed: {
+    monsterBarStyles() {
+      return {width: this.monsterHealth + "%"};
+    },
+    PlayerBarStyles() {
+      return {width: this.playerHealth + "%"};
+    },
+  },
   methods: {
     attackMonster() {
       const attackValue = getRandomValue(5, 12);
