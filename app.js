00038ee92dc09fee2eb534d7baa84a255aac62de
vahid-bagh3ri <70237190+vahid-bagh3ri@users.noreply.github.com>
2021-05-12 11:48:20 +0430

    9. Vue App Lifecycle - Practice

--- app.js
+++ app.js
@@ -17,9 +17,37 @@ const app = Vue.createApp({
       // console.dir(this.$refs.userText);
     },
   },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmonuted()");
+  },
 });
 
 app.mount("#app");
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
 
 // ...
 
@@ -50,7 +78,7 @@ const proxy = new Proxy(data, handler);
 
 proxy.message = "Helooo!!!!";
 
-console.log(proxy.longMessage);
+// console.log(proxy.longMessage);
 
 const app2 = Vue.createApp({
   template: `
