aeeb714ff82ca7b8a933b5c93c37e605c85a0d35
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-16 15:45:02 +0430

    7. Slot Styles & Compilation

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -11,6 +11,12 @@
 export default {};
 </script>
 <style scoped>
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 div {
   margin: 2rem auto;
   max-width: 30rem;
