24e902d533aca4521e5cf343e8cc40577b017287
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-17 11:37:58 +0430

    2. Setup & First Steps

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'officail-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentaion',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    };
+  }
+};
+</script>
