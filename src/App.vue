965522dd1f82cce03a0ca7a2e1ad9d7b1ea038bd
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-17 14:11:07 +0430

    5. Header & BaseCard Components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import storedResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
   components: {
-    storedResources
+    storedResources,
+    TheHeader
   },
   data() {
     return {
