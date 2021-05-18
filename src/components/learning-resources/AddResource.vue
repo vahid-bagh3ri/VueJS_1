895a05caaa198aea6fe4d9cd775dd38e8111955c
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-18 10:25:35 +0430

    9. Adding a Form // Add markup and functionality

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,7 +1,28 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" name="link" id="link" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
 
 <script>
-export default {};
+import BaseButton from '../UI/BaseButton.vue';
+export default {
+  components: { BaseButton }
+};
 </script>
