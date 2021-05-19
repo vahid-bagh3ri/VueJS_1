be717b20744cb43310c8ae342c8e591a16b83d07
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-19 19:32:44 +0430

    start module 11

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
@@ -11,4 +14,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BaseCard from './components/UI/BaseCard.vue';
-import BaseButton from './components/UI/BaseButton.vue';
-import BaseDialog from './components/UI/BaseDialog.vue';
 
-import StoredResources from './components/learning-resources/StoredResources.vue';
-const app = createApp(App);
-
-app.component('base-card', BaseCard);
-app.component('base-button', BaseButton);
-app.component('base-dialog', BaseDialog);
-app.component('stored-resources', StoredResources);
-
-app.mount('#app');
+createApp(App).mount('#app');
