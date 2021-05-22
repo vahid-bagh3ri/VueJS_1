c01b486cb4fa61f72b9dcaad721bfad37cbb0307
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-22 16:34:54 +0430

    8. Getting Data (GET Request) & Transforming Response Data // Final // Get data From database and show it on the screen

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -36,13 +36,21 @@ export default {
       fetch(
         'https://vue-http-demo-60b83-default-rtdb.firebaseio.com/surveys.json'
       )
-        .then(function(response) {
+        .then(response => {
           if (response.ok) {
             return response.json();
           }
         })
-        .then(function(data) {
-          console.log(data);
+        .then(data => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating
+            });
+          }
+          this.results = results;
         });
     }
   }
