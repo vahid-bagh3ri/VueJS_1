a49d610b5da6cc6508e63f80200e3fdb198c4534
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-22 16:57:27 +0430

    10. Showing a Loading... Message

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -28,11 +29,13 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading: false
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(
         'https://vue-http-demo-60b83-default-rtdb.firebaseio.com/surveys.json'
       )
@@ -42,6 +45,7 @@ export default {
           }
         })
         .then(data => {
+          this.isLoading = false;
           const results = [];
           for (const id in data) {
             results.push({
