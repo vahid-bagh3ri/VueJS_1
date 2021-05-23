e6d94b29dabc85b9a8cc12cab52aba8b0c2e6e15
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-05-23 12:36:52 +0430

    3. Routing Setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: []
+});
 
 const app = createApp(App);
 
