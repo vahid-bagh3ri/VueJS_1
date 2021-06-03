9980ecfe2c9686000aa026bb4662d7e40c4cb83b
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-03 12:45:37 +0430

    14. Using Nested Routes

--- src/main.js
+++ src/main.js
@@ -11,9 +11,14 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList }, // alias: '/'  //our-domain.com/teams => TeamsList
+    {
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        { path: ':teamId', component: TeamMembers, props: true } // /teams/t1
+      ]
+    }, //our-domain.com/teams => TeamsList] }, // alias: '/'  //our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList }, //our-domain.com/teams => UsersList
-    { path: '/teams/:teamId', component: TeamMembers, props: true }, //our-domain.com/teams => TeamsList
     { path: '/:notFound(.*)', component: NotFound }
   ]
 });
