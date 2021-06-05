f284f0a72d321e6e5b4a26bae5d4646c1f40d44b
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-05 20:39:07 +0430

    21. The Global afterEach Guard

--- src/main.js
+++ src/main.js
@@ -59,6 +59,11 @@ router.beforeEach(function(to, from, next) {
   // }
   next();
 });
+router.afterEach(function(to, from) {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
 
 const app = createApp(App);
 
