b17a3ab18a0f336d7c7bc731a901a65c1875bb8d
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-05 21:04:12 +0430

    22. Beyond Entering Route Leave Guards

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="savedChanges">Save Changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -18,16 +19,38 @@ export default {
     UserItem
   },
   inject: ['users'],
+  data() {
+    return { changedSaved: false };
+  },
   methods: {
     confirmInput() {
       // do something
       this.$router.push('/teams');
+    },
+    savedChanges() {
+      this.changedSaved = true;
     }
   },
   beforeRouteEnter(to, from, next) {
     console.log('UsersList Cmp beforeRouteEnter');
     console.log(to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList Cmp beforeRouteLeave');
+    console.log(to, from);
+
+    if (this.changedSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm(
+        'Are you sure? You got unsaved changes!'
+      );
+      next(userWantsToLeave);
+    }
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 };
 </script>
