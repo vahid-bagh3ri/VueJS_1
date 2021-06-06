c558681891257481b0e245b83cde1ec6bff3bf86
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-06 10:43:13 +0430

    Start module 14 // File added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import router from './router.js';
+import BaseModal from './components/BaseModal.vue';
 
 const app = createApp(App);
 
-app.use(router);
+app.component('base-modal', BaseModal);
 
 app.mount('#app');
