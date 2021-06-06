1b19bccdfd9694997ea9bdf3f98e52a47c575ffb
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-06 14:53:00 +0430

    6. Using the Transition Component

--- src/App.vue
+++ src/App.vue
@@ -93,6 +93,29 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(+30px);
+}
 @keyframes slide-fade {
   0% {
     transform: translateX(0) scale(1);
