4b059fd5f41413dff6eeba3546915f1eb27c8fe8
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-14 11:02:40 +0430

    8. Running Async Code with Actions

--- src/main.js
+++ src/main.js
@@ -22,6 +22,9 @@ const store = createStore({
       setTimeout(function() {
         context.commit('increment');
       }, 2000);
+    },
+    increase(context, payload) {
+      context.commit('increase', payload);
     }
   },
   getters: {
