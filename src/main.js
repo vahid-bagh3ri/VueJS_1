30386e5cdd0a663f32bbc73a8bcad293bf7429d9
Vahid Bagheri <vahid.bagheri1379@gmail.com>
2021-06-15 14:01:00 +0430

    16.1 vuex-11-a-challenge-starting-code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 
+import router from './router.js';
 import App from './App.vue';
-import store from './store/index.js';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(store);
+app.use(router);
+
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
